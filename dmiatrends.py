b101e0a083cb38ca7ceabaf53ed918f01bb9a4f6
lehtosaarinen <32978813+lehtosaarinen@users.noreply.github.com>
2017-10-21 13:05:32 +0300

    Add files via upload

--- dmiatrends.py
+++ dmiatrends.py
@@ -1,6 +1,7 @@
 # ребята, я не забыл
 # мож завтра еще пришлю =)
 
+#1
 def power_sum(l,r,p = 1.0):
     i = l
     mysum = 0
@@ -8,3 +9,19 @@ def power_sum(l,r,p = 1.0):
         mysum += i ** p
         i += 1
     return mysum
+
+#2
+def solve_equation(a,b,c):
+	if a == 0 & b == 0 & c == 0:
+		print('inf')
+	else:
+		D = b*b - 4*a*c
+		if D > 0:
+			return ((-b - D ** 0.5)/(2*a),
+				(-b + D ** 0.5)/(2*a))
+		elif D == 0:
+			return -b / 2 / a
+		else:
+			return None
+
+#3
